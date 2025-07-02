901898e2583484290976e7247322fcad2af5e9d9
Dal ferro Christian <christiandalferro@gmail.com>
2025-07-02 10:59:02 +0200

    recup

--- cart.js
+++ cart.js
@@ -1,14 +1,39 @@
 function divTrajet(obj){
     console.log(obj)
+
     let divTrajet = document.createElement('div');
     divTrajet.setAttribute('class','trajet');
-    divTrajet.textContent = `a`
+    divTrajet.textContent = obj.departure+'>'+obj.arrival;
+
+
+    let divDate = document.createElement('div');
+    divDate.setAttribute('class','date');
+    const objDate = newDate(obj.date)
+    
+    a.getUTCHours() , a.getMinutes()
+
+
+    divDate.textContent = obj.date
 
 
 
 
 
 }
+
+_id: "6863db055b3d2f1c31535bd7"
+​​​​
+arrival: "Bruxelles"
+​​​​
+date: "2025-07-01T08:16:16.993Z"
+​​​​
+departure: "Marseille"
+​​​​
+price: 73
+
+
+
+
 
 fetch('http://localhost:3000/reservations/recup')
         .then(response=>response.json())
